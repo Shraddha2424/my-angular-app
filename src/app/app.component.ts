0bf011bb42c5184186f1343dbc2f1871c4d878ea
Shraddha2424 <shraddha.ajabe@waiin.com>
2025-03-26 13:53:07 +0530

    all files commit to main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports : [LoginComponent,ProfileComponent],
+  // template: `<h1>Hello World from Angular 19!</h1>`,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl:'./app.component.css',
+  standalone: true
+   
 })
 export class AppComponent {
-  title = 'my-angular-app';
+//   title = 'Angular 19 version'
+//   // name = "shraddha ajabe"
+  
+//   handleClickEvent(){
+//    alert("function callled")
+//    this.otherFunction()
+//   }
+//    otherFunction(){
+//     alert("other func")
+//    }
+
+//    name:string="anil sidhu"
+//    updateName(){
+//     this.name="sidhu"
+//     this.sum(40,30)
+//    }
+//    sum(a:number,b:number){
+// alert(a+b);
+
+//    }
+  //  
+ count=0
+  handleCounter(val:string){
+    if(val=='minus'){
+      this.count=this.count-1
+    }else if(val =='plus'){
+      this.count=this.count+1
+    }else{
+      this.count=0
+    }
+  
+  }
+  // event listener
+
+  handleEvent(event:Event){
+    console.log("function called",event.type);
+    // console.log("function called",(event.target as Element).className);
+
+
+  }
+
+
+
+
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component'; // ✅ Use full path to app folder
+import { appConfig } from './app/app.config'; // ✅ Use full path to app folder
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch(err => console.error(err));
